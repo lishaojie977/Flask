e8d0174433e7022d52c7f897a5e275e5448b503c
lishaojie977 <1171987472@qq.com>
2020-04-03 14:00:01 +0800

    增加修改功能

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect,request,url_for
 import click
 from flask_sqlalchemy import SQLAlchemy   #导入扩展类
 
@@ -17,6 +17,8 @@ app = Flask(__name__)
 #windows
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
 app.config['SQLALCHY_TRACK_MODIFICATIONS'] = False  #关闭了对模型修改的监控
+app.config['SECRET_KEY'] = 'watchlist_dev'
+
 db = SQLAlchemy(app)   #初始化扩展，传入程序实例app
 
 #models
@@ -35,15 +37,50 @@ def common_user():
     user = User.query.first()
     return dict(user=user)
 
+
 #views
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        # request在请求触发的时候才会包含数据
+        title = request.form.get('title')
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('不能为空或超过最大长度')
+            return redirect(url_for('index'))
+        # 保存表单数据
+        movie = Movie(title=title,year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('添加成功')
+        return redirect(url_for('index'))
+
     movies = Movie.query.all()
     return render_template('index.html',movies=movies)
 
 
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        # 验证数据
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('不能为空或超过最大长度')
+            return redirect(url_for('index'))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('更新完成')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html',movie=movie)
+
 
 #自定义命令
+#新建data.db的数据库初始化功能
 @app.cli.command()  #装饰器，注册命令
 @click.option('--drop',is_flag=True,help='删除后在创建')
 def initdb(drop):
